5a9ee6c16e71aabb2ebd2febf427b35efbf3c4a7
shahid <shahidudy@gmail.com>
2022-10-31 08:54:15 +0530

    first commit

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import {baseUrL} from "./constants/constants"
+
+const instance = axios.create({
+    baseURL : baseUrL,
+  });
+
+  export default instance
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrL = 'https://api.themoviedb.org/3';
+export const API_KEY = 'a7795b8447ef87128b569161beef5357';
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
